031ca745eb5be9d007a57aa313ac3243d8d26653
Rodrigo <rodricelis@gmail.com>
2024-05-19 19:22:46 -0400

    add env a next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,8 @@ const nextConfig = {
         NEXT_GOOGLE_CLIENT_SECRET: process.env.NEXT_GOOGLE_CLIENT_SECRET,
         NEXT_GITHUB_CLIENT_ID: process.env.NEXT_GITHUB_CLIENT_ID,
         NEXT_GITHUB_CLIENT_SECRET: process.env.NEXT_GITHUB_CLIENT_SECRET,
-        DATABASE_URL: process.env.DATABASE_URL
+        DATABASE_URL: process.env.DATABASE_URL,
+        NEXT_GOOGLE_ANALITYCS: process.env.NEXT_GOOGLE_ANALITYCS,
     },
     images: {
         domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
